e5a3ab6bcfac4abe0b3cff5433fd23f80ccf491e
Haqvevu72 <novruzluqyds20@gmail.com>
2024-08-10 18:08:38 +0400

    vetSlice is added

--- PetCare_Project/src/Store/store.jsx
+++ PetCare_Project/src/Store/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import messagesSlice from "./messagesSlice";
 import chatSlice from "./chatSlice";
+import vetsSlice from "./vetsSlice";
 
 const applicationStore = configureStore({
   reducer: {
     messages: messagesSlice,
-    chat: chatSlice
+    chat: chatSlice,
+    vets: vetsSlice
   },
 });
 
---
+++ PetCare_Project/src/Store/vetsSlice.jsx
@@ -0,0 +1,29 @@
+import react from "react";
+import { createSlice } from "@reduxjs/toolkit";
+import VetsFetch from "../AxiosFetchs/EntityFetchs/VetsFetch";
+
+const vetsSlice = createSlice({
+  name: "vets",
+  initialState: {
+    vetsArray: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(VetsFetch.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(VetsFetch.fulfilled, (state, action) => {
+      state.vetsArray = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(VetsFetch.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = true;
+    });
+  },
+});
+
+export const {} = vetsSlice.actions;
+export default vetsSlice.reducer;
