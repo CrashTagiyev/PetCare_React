b69af64d204d8056e11085af9a237ac6344634a6
Haqvevu72 <novruzluqyds20@gmail.com>
2024-08-18 14:16:22 +0400

    style/vetinfo style is updated

--- PetCare_Project/src/Components/usersInformationCOmponents/VetInfo/Vetinfo.jsx
+++ PetCare_Project/src/Components/usersInformationCOmponents/VetInfo/Vetinfo.jsx
@@ -7,6 +7,7 @@ import { CreateChatConnection } from "../../../AxiosFetchs/AuthFetchs/ChatConnec
 import { FetchVet } from "../../../AxiosFetchs/EntityReduxFetchs/FetchVet";
 import { useAuth } from "../../../Hooks/useAuth";
 import { useLocalStorage } from "../../../Hooks/useLocalStorage";
+import "../VetInfo/vetInfo.scss";
 
 const Vetinfo = () => {
   const [info, setInfo] = useState({});
@@ -25,22 +26,76 @@ const Vetinfo = () => {
 
     getVetsInfo();
   }, []);
-  const handleWriteMessageButton =async (e) => {
+  const handleWriteMessageButton = async (e) => {
     e.preventDefault();
     if (user) {
-      await CreateChatConnection(user.username, info.userName + "+" + user.username);
+      await CreateChatConnection(
+        user.username,
+        info.userName + "+" + user.username
+      );
       setActiveTab("Inbox");
       navigate("/userprofileinfo");
     }
   };
 
   return (
-    <>
-      <Button type="primary" onClick={handleWriteMessageButton}>
-        Write message
-      </Button>
-      <h1>{info.firstname}</h1>
-    </>
+    <div className="vet-info-cont">
+      <div className="vet-profile">
+        <img src={info.profileImageUrl}></img>
+      </div>
+      <div className="vet-info">
+        <div className="vet-single-info">
+          <div>
+            <p className="vet-label">Firstname:</p>
+          </div>
+          <div>
+            {" "}
+            <p className="vet-act-info">{info.firstname}</p>
+          </div>
+        </div>
+        <div className="vet-single-info">
+          <div>
+            <p className="vet-label">Address:</p>
+          </div>
+          <div>
+            {" "}
+            <p className="vet-act-info">{info.address}</p>
+          </div>
+        </div>
+        <div className="vet-single-info">
+          <div>
+            <p className="vet-label">Username:</p>
+          </div>
+          <div>
+            {" "}
+            <p className="vet-act-info">{info.userName}</p>
+          </div>
+        </div>
+      </div>
+      <div className="vet-info">
+        <div className="vet-single-info">
+          <div>
+            <p className="vet-label">Lastname:</p>
+          </div>
+          <div>
+            {" "}
+            <p className="vet-act-info">{info.lastname}</p>
+          </div>
+        </div>
+        <div className="vet-single-info">
+          <div>
+            <p className="vet-label">City:</p>
+          </div>
+          <div>
+            {" "}
+            <p className="vet-act-info">{info.city}</p>
+          </div>
+        </div>
+        <div className="write-to-vet">
+          <button onClick={handleWriteMessageButton}>Write to vet</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
